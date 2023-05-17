0882b2cc601410407ab2d74353c7dd12daf8730d
Shatvik Pandey <75732635+shatvik@users.noreply.github.com>
2023-05-17 16:02:29 +0530

    fixed  cors

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,9 @@ connectDB();
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://realtime-chat-app-t4vb.vercel.app')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS, PUT, HEAD')
   next()
 })
 app.use(express.json()); // to accept json data
